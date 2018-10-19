c292fdf51fe7f6c0b9f67d884ec8fba05c11c53c
hitt08 <hitt_08@hotmail.com>
2018-10-19 04:57:03 +0100

    Solution for Question6

--- q6.py
+++ q6.py
@@ -4,5 +4,32 @@ import numpy as np
 
 def question06(numServers, targetServer, times):
   # modify and then return the variable below
-  answer = -1
+  if(targetServer==0):
+      answer=0
+  else :
+      neighbours=[n for n in range(1,numServers)]
+      neighbours.remove(targetServer)
+      dist=times[0,targetServer]
+      for i in neighbours:
+          c=times[0,i]
+          if(c<dist):
+              c_neighbours=list(neighbours)
+              c_neighbours.remove(i)
+              c=min_dist(c_neighbours,targetServer,i,times,c)
+          dist=c if(c<dist) else dist
+      answer = dist
   return answer
+
+def min_dist(neighbours,targetServer,srcServer,times,dist):
+    c=dist+times[srcServer,targetServer]
+    if(len(neighbours)==0):
+        min=c
+    else :
+        min=c
+        for n in neighbours:
+            new_neighbours=list(neighbours)
+            new_neighbours.remove(n)
+            new_dist=min_dist(new_neighbours,targetServer,n,times,dist+times[srcServer,n])
+            if(new_dist<min):
+                min=new_dist
+    return(min)
